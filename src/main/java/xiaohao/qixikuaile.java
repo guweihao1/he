c3a9bb3ae993b1b5ccbfc61b56c56dedd5ac706b
guweihao <weihao.gu@pcitc.com>
2017-09-08 13:48:35 +0800

    首次提交

---
+++ src/main/java/xiaohao/qixikuaile.java
@@ -0,0 +1,10 @@
+package xiaohao;
+
+public class qixikuaile {
+	public static void main(String[] args) {
+		System.out.println("豪宝七夕快乐啊，虽然你在北京，我在西安，但是我们永远心连心！！！");
+		System.out.println("豪宝加油，你是最棒的！");
+	}
+	
+
+}
